dd970187ac7fdec0bc8d7901b6f081845b9ebd6b
Kelen Vargas <kelenvargas.senac@gmail.com>
2024-03-07 10:14:58 -0300

    Incluindo a logica das funções e add o dados do json

--- BancoTech/app.js
+++ BancoTech/app.js
@@ -1,14 +1,30 @@
+import dados from '../data.json';
+
 const prompt = require('prompt-sync')(); // Configuração do prompt
 let opcao = 0;
 
+
 // Programa exibindo o menu principal.
 function iniciarPrograma() {
-
     do {
 
         exibirMenu();
 
     } while (opcao != 0)
+}
+
+function continuar() {
+    console.log("Gostaria de continuar no programa? ");
+    let opcao2;
+    do {
+        opcao2 = prompt("S/N").toUpperCase();
+        if (opcao2 == 'N') {
+            opcao = 0
+        } else if (opcao2 !== 'S') {
+            console.log('Opção inválida. Tente novamente.');
+        }
+
+    } while (opcao2 !== 'N' && opcao2 !== 'S')
 }
 
 // Chama a função que iniciará o programa
@@ -25,20 +41,50 @@ function exibirMenu() {
     console.log("3) Remover conta");
     console.log("0) Sair do programa");
     opcao = prompt("Escolha uma opção: ");
+    console.clear();
     processarOpcao(opcao);
-
+    continuar();
+    console.clear();
 }
 
 // Funções para cada opção do menu que o usuário vai escolher
 function acessarConta() {
+    //transformando os dados do json em um array de contas
+    const contas = dados.contas;
+
     console.log('Bem Vindo a sua conta Tech!');
-    nome = prompt("Digite seu nome: ");
+    // armazenando os dados
+    const contaInformada = prompt("Digite sua conta: ");
+    const senhaInformada = prompt("Digite sua senha: ");
+
+    //conta encontrada
+    // transformar em uma função
+    const contaEncontrada = contas.find(conta => { return conta.numeroConta === contaInformada && conta.senha === senhaInformada; });
+
+
+    // Função autenticar usuário : 
+    //caso a conta e senha informados sejam válidas, mostrar menu minha conta
+    // (ver saldo, sacar, depositar, ver informações do cliente, alterar senha e sair), 
+    //caso seja inválida pedir para o usuário informar novamente
     // Lógica da opção acessar conta
 }
 
 function criarConta() {
     console.log('Crie sua conta em segundos!');
+    dados.contas.push(novoUsuario); // adcionando novo, mandando pro json
+
+    const novoUsuario = {
+        "id": novoId,
+        "nome": nome,
+        "senha": senha,
+        "tipoConta": tipoConta,
+        "agencia": agencia,
+        "conta": conta,
+        "saldo": parseFloat(saldoInicial) || 0.00
+    };
+
     // Lógica da opção criar conta
+
 
 }
 
